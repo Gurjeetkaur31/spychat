17a99436cfb8db126563e7bd09cecb659aef9c7f
Gurjeetkaur31 <gurjeetsingh1209@gmail.com>
2018-01-01 09:48:20 +0300

    My first spychat commit

---
+++ main.py
@@ -0,0 +1,13 @@
+print 'Let\'s get started'
+
+
+spy_name = raw_input("Welcome to spy chat, you must tell me your spy name first:\n ")
+
+
+print 'Welcome ' + spy_name + '. Glad to have you back with us.'
+
+spy_salutation = raw_input("Should I call you (Mr./Miss./Mrs.)?: ")
+
+spy_name = spy_salutation + " " + spy_name
+
+print 'Alright '+ spy_name +' I\'ll like to know a little bit more about you...'
